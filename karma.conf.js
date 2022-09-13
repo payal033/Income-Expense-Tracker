6c0bffb141fe6f54c7f55c9aefbc2d9714168d64
Payal <payalkarwa99@gmail.com>
2022-09-14 00:11:19 +0530

    update names

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/money-manager'),
+      dir: require('path').join(__dirname, './coverage/Income-Expense-Tracker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
